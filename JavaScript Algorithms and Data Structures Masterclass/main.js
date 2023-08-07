ed14bb096e81602de059d1f2ec26af45f889a974
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2023-08-07 23:44:59 +0300

    JavaScript Algorithms and Data Structures Masterclass/S1:L9[Simplifying Big O Expressions]]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -86,3 +86,34 @@ function printAllPairs (n) { // [O(n²)] >> O(n) Operation inside of O(n) Operat
         }
     }
 }
+
+// L9: Simplifying Big O Expressions
+
+/*
+    - O(2n) >> O(n)
+    - O(500) >> O(1)
+    - O(15n²) >> O(n²)
+    - O(n + 10) >> O(n)
+    - O(1000n + 50) >> O(n)
+    - O(n² + 5n + 8) >> O(n²)
+*/
+
+/*
+    >> Big O Shorthands
+        1. Arithmetic operations are constant
+        2. Variable assignment is constant
+        3. Accessing elements in an array (by index) or object (by key) is constant
+        4. In a loop, the the complexity is the length of the loop times the complexity of whatever happens inside of the loop
+*/
+
+function logAtLeast5 (n) { // [O(n)]
+    for (let i = 1; i <= Math.max(5, n); i++) {
+       console.log(i);
+    }
+}
+
+function logAtMost5 (n) { // [O(1)]
+    for (let i = 1; i <= Math.min(5, n); i++) {
+        console.log(i);
+    }
+}
