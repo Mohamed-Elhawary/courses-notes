6bcd0fbc12f07fb1e4c23c6c91c94c147fa997db
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-03 18:11:34 +0300

    JavaScript Algorithms and Data Structures Masterclass/S4:L19[Introduction to Problem Solving]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -4,7 +4,7 @@
 
 function addUpToSolution1(n) {
     let total = 0;
-    for(i = 1; i <= n; i++) {
+    for(let i = 1; i <= n; i++) {
         total += i;
     }
 
@@ -21,7 +21,7 @@ function addUpToSolution2(n) {
 console.log(addUpToSolution2(6));
 
 /*
-    > What does better solution mean ?
+    >> What does better solution mean?
         - Faster ? [Will focus on this]
         - Less Memory ?
         - More Readable ?
@@ -41,30 +41,30 @@ console.log(`Time Elapsed: ${(time2Solution2- time1Solution2) / 1000} seconds.`)
 // L7: Counting Operations
 
 /* 
-    - In addUpToSolution1 we have:
+    >> In addUpToSolution1 we have:
         - 2 assignment (let total = 0; & let i = 0)
         - n comparisons (i <= n)
         - n additions and n assignments (i ++ & total += i)
 
-    - In addUpToSolution2 we have:
+    >> In addUpToSolution2 we have:
         - 3 operations (*, +, /) only
 
-    >> Depending on what we count, the number of operations can be as low as (2n) or as high as (5n + 2)
+    > Depending on what we count, the number of operations can be as low as (2n) or as high as (5n + 2)
 */
 
 
 // L8: Intro to Big O
 
 /* 
-    >> Big O Definition
-        We say that an algorithm is O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases.
+    >> Big O Definition:
+        > We say that an algorithm is O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases.
             - f(n) could be linear (f(n) = n)
             - f(n) could be quadratic (f(n) = n²)
             - f(n) could be constant (f(n) = 1)
             - f(n) could be something entirely different!
 
-    >> addUpToSolution1: Number of operations is eventually bounded by a multiple of n (say: 10n) [O(n)]
-    >> addUpToSolution2: always 3 operations [O(1)] 
+    > addUpToSolution1: Number of operations is eventually bounded by a multiple of n (say: 10n) [O(n)]
+    > addUpToSolution2: always 3 operations [O(1)] 
 */
 
 function countUpAndDown (n) { // [O(n)]
@@ -81,7 +81,7 @@ function countUpAndDown (n) { // [O(n)]
 
 function printAllPairs (n) { // [O(n²)] >> O(n) Operation inside of O(n) Operation
     for (let i = 0; i < n; i++) { // [O(n)]
-        for (let j = 0; j< n; j++) { // [O(n)]
+        for (let j = 0; j < n; j++) { // [O(n)]
             console.log(i, j);
         }
     }
@@ -96,14 +96,12 @@ function printAllPairs (n) { // [O(n²)] >> O(n) Operation inside of O(n) Operat
     - O(n + 10) >> O(n)
     - O(1000n + 50) >> O(n)
     - O(n² + 5n + 8) >> O(n²)
-*/
 
-/*
-    >> Big O Shorthands
-        1. Arithmetic operations are constant
-        2. Variable assignment is constant
-        3. Accessing elements in an array (by index) or object (by key) is constant
-        4. In a loop, the the complexity is the length of the loop times the complexity of whatever happens inside of the loop
+    >> Big O Shorthands:
+        1. Arithmetic operations are [constant]
+        2. Variable assignment is [constant]
+        3. Accessing elements in an array (by index) or object (by key) is [constant]
+        4. In a loop, the complexity is the length of the loop times the complexity of whatever happens inside of the loop
 */
 
 function logAtLeast5 (n) { // [O(n)]
@@ -122,8 +120,7 @@ function logAtMost5 (n) { // [O(1)]
 
 /* 
     >> We can use big O notation to analyze space complexity, how much additional memory do we need to allocate in order to run the code in our algorithm
-
-        - Most primitives (booleans, numbers, undefined, null) are constant space
+        - Most primitives (booleans, numbers, undefined, null) are [constant] space
         - Strings require O(n) space (where n is the string length)
         - Reference types are generally O(n), where n is the length (for arrays) or the number of keys (for objects)
 */
@@ -148,16 +145,12 @@ function double (arr) { // [O(n)] Space
 
 /*
     >> what's a log?
-        
         - log₂(8) = 3  >>>>  2 power of (3) = 8 
         - log₂(value) = exponent >>>> 2 power of (exponent) = value
 
-    > The logarithm of a number roughly measures the number of times you can divide that number by 2 before you get a value that's less than or equal to one.
-*/
+    > The Algorithm of a number roughly measures the number of times you can divide that number by 2 before you get a value that's less than or equal to one.
 
-/*
     >> Who uses log in complexity ??
-
         - Certain searching algorithms have logarithmic (time) complexity.
         - Efficient sorting algorithms involve logarithms.
         - Recursion sometimes involves logarithmic (space) complexity.
@@ -187,10 +180,8 @@ function double (arr) { // [O(n)] Space
         * Searching - O(N)
         * Access - 0(1)
         
-        -   When you don't need any ordering,objects are an excellent choice!, because of constant time for insertion, removal and accessing data.
-*/
+    > When you don't need any ordering, objects are an excellent choice!, because of constant time for insertion, removal and accessing data.
 
-/*
     >> Big O of Object Methods
         - Object.keys - O(N)
         - Object.values - O(N)
@@ -212,11 +203,11 @@ function double (arr) { // [O(n)] Space
         * Searching O(N)
         * Access - O(1)
         
-        -   Inserting item at the end of the array is faster than inserting at the beginning, because inserting at the end will just require adding a new index at the end of the array after the last existing index,
-            but inserting at the beginning will require re-ordering the array items and shifting their index for each item, where index (Zero) now will be related to the new inserted item, so the amount of time it takes to inserting 
-            item at the beginning of array, roughly grows in proportion with the size of the array. 
+    > Inserting item at the end of the array is faster than inserting at the beginning, because inserting at the end will just require adding a new index at the end of the array after the last existing index,
+      but inserting at the beginning will require re-ordering the array items and shifting their index for each item, where index (Zero) now will be related to the new inserted item, so the amount of time it takes to inserting 
+      item at the beginning of array, roughly grows in proportion with the size of the array. 
 
-        -   Same time complexity and same logic goes for removing item from the end and from the beginning of the array.
+    > Same time complexity and same logic goes for removing item from the end and from the beginning of the array.
 */
 
 
@@ -224,14 +215,33 @@ function double (arr) { // [O(n)] Space
 
 /*
     >> Big O of Array Operations
+        • push - 0(1)
+        • pop - 0(1)
+        • shift - O(N)
+        • unshift - O(N) 
+        • concat - O(N)
+        • slice - O(N)
+        • splice - O(N)
+        • sort - O(N * log N)
+        • forEach/map/fill/reduce/etc. - O(N)
+*/
 
-    • push - 0(1)
-    • pop - 0(1)
-    • shift - O(N)
-    • unshift - O(N) 
-    • concat - O(N)
-    • slice - O(N)
-    • splice - O(N)
-    • sort - O(N * log N)
-    • forEach/map/fil know.educe/etc. - O(N)
+/* Section 4: Problem Solving Approach */
+
+// L19: Introduction to Problem Solving
+
+/* 
+    >> What is Algorithm?
+        - A process or set of steps to accomplish a certain task.
+
+    >> How do you improve in solving problems?
+        - Devise a plan for solving problems
+        - Master common problem solving patterns
+    
+    >> Problem Solving Steps:
+        - Understand the problem
+        - Explore Concrete Examples
+        - Break it down
+        - Solve/Simplify
+        - Look back and refactor
 */
