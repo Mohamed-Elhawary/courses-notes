b2cd410452ef498b9b93dd202a342096ffc55e20
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-04 23:02:25 +0300

    JavaScript Algorithms and Data Structures Masterclass/S4:L22[Step 3: Break It Down]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -267,3 +267,27 @@ function double (arr) { // [O(n)] Space
         • Explore Examples with Empty Inputs
         • Explore Examples with Invalid Inputs
 */
+
+// L22: Step 3: Break It Down
+
+/*
+    >> BREAK IT DOWN ?
+        - Explicitly write out the steps you need to take. This forces you to think about the code you'll write before you write it, 
+          and helps you catch any lingering conceptual issues or misunderstandings before you dive in and have to worry about details (e.g. language syntax) as well.
+
+    > Write a function which takes in a string and returns count of each character in the string.
+        * Examples:
+        ------------
+            - charCount("aaaa"); // {a:4}
+            - charCount("hello"); // {h:1, e:1, l:2, o:1}
+            - charCount("Your PIN number is 1234!"); // {1:1, 2:1, 3:1, 4:1, b:1, e:1, i:2, m:1, n:2, o:1, p:1, r:2, s:1, u:2, y:1}
+*/
+
+    function charCount(str) { // Break the steps of the problem down
+        // make object to return at end
+        // loop over string, for each character...
+        // if the char is a number/letter AND is a key in object, add one to count
+        // if the char is a number/letter AND not in object, add it to object and set value to 1
+        // if character is something else (space, period, etc. ) don't do anything
+        // return object at end
+    }
