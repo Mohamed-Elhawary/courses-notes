409daa0b4e045870de2dd4a1bb90fd97c5f4ac9b
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-07 19:18:56 +0300

    JavaScript Algorithms and Data Structures Masterclass/S4:L23[Step 4: Solve Or Simplify]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -312,3 +312,35 @@ function double (arr) { // [O(n)] Space
     }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L23: Step 4: Solve Or Simplify
+
+/*
+    >> SIMPLIFY
+    - Find the core difficulty in what you're trying to do
+    - Temporarily ignore that difficulty
+    - Write a simplified solution
+    - Then incorporate that difficulty back in
+*/
+
+function charCount(str) {
+    // make object to return at end
+    var result = {};
+    // loop over string, for each character...
+    for(var i = 0; i < str.length; i++) {
+        var char = str[i].toLowerCase();
+        //if the char is a number/letter AND is a key in object, add one to count
+        if (result [char] > 0) {
+            result [char]++;
+        }
+        //if the char is a number/letter AND not in object, add it to object and set value t
+        else {
+            result [char] = 1;
+        };
+    }
+    //if character is something else (space, period, etc.) don't do anything
+    // return object at end
+    return result;
+}
+
+/*---------------------------------------------------------------------------------------------------*/
