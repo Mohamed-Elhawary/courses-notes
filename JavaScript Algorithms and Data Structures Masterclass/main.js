bde78307769de88f30e183991904aafabf5ef784
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-09 20:37:37 +0300

    JavaScript Algorithms and Data Structures Masterclass/S4:L24[Step 5: Look Back and Refactor]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -338,9 +338,71 @@ function charCount(str) {
             result [char] = 1;
         };
     }
-    //if character is something else (space, period, etc.) don't do anything
+    //if character is something else (space, period, etc.) don't do anything [Maybe this part is difficult, I will ignore it for later but keep the comment for this part to remember it]
     // return object at end
     return result;
 }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L24: Step 5: Look Back and Refactor
+
+/*
+    >> REFACTORING QUESTIONS:
+        • Can you check the result?
+        • Can you derive the result differently?
+        • Can you understand it at a glance?
+        • Can you use the result or method for some other problem?
+        • Can you improve the performance of your solution?
+        • Can you think of other ways to refactor?
+        • How have other people solved this problem?
+*/
+
+function charCount(str) {
+    var obj = {};
+        for (var i = 0; i < str.length; i++) {
+            var char = str[i].toLowerCase();
+            if (/[a-z0-9]/.test(char)) {
+                if (obj [char] > 0) {
+                    obj [char] ++;
+                } else {
+                    obj [char] = 1;
+                };
+            }
+        }
+    return obj;
+}
+
+/*
+    >> Refactoring Code:
+        - using "for of" loop 
+        - using one line condition in obj[char] instead of "if else" condition
+        - using built-in js function to check if the "char" is [number/string] instead of "Regex".
+            {The used built-in method below is faster than Regex by 55%}
+        - lower case any char after checking of its AlphaNumeric state, to not convert all the string chars into lowercase even if non AlphaNumeric chars.
+*/
+
+function charCount(str) { 
+    var obj = {};
+    for (var char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+    return obj;
+}
+
+function isAlphaNumeric (char){
+    var code = char.charCodeAt(0);
+    if (
+        !(code> 47 && code <58) && // numeric (0-9)
+        ! (code> 64 && code < 91) && // upper alpha (A-Z)
+        ! (code> 96 && code < 123) // lower alpha (a-z)
+    ) { 
+        return false;
+    }
+
+    return true;
+}
