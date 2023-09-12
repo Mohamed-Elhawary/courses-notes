f82e7cc07657de82532d209d6a2892a5c9fb2c0c
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-12 18:29:46 +0300

    JavaScript Algorithms and Data Structures Masterclass/S5:L31[Anagram Challenge Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -571,3 +571,47 @@ function validAnagram2(str1, str2){
 }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L31: Multiple Pointers Pattern
+
+/*
+    >> Multiple Pointers Pattern:
+        - Creating pointers or v s that correspond to an index or position and owards the beginning, end or middle base a certain condition.
+        - Very efficient for solving problems with minimal space complexity as well
+        
+        > Write a function called sumZero which accepts a "sorted" array of integers. The function should find the first pair
+          where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+            * Examples:
+            ------------
+            - sumZero([-3, -2,-1,0,1,2,3]) // [-3,3]
+            - sumZero([-2,0,1,3]) // undefined
+            - sumZero([1,2,3]) // undefined
+*/
+
+function sumZero1(arr) { // This is a NAIVE solution, means [Not the best possible solution in this case] >> Time Complexity is [O(n^2)]
+    for(let i = 0; i < arr.length; i++){
+        for(let j =i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+function sumZero2(arr) { // [O(n)]
+    let left = 0;
+    let right = arr. length - 1;
+    
+    while(left < right) {
+        let sum = arr[lēft] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+/*---------------------------------------------------------------------------------------------------*/
