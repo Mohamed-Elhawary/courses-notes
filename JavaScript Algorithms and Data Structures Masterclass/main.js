c494a57d312afced23113511b240c7e3245565fc
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-09-12 17:38:26 +0300

    JavaScript Algorithms and Data Structures Masterclass/S5:L30[Anagram Challenge Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -511,3 +511,63 @@ function same2(arr1, arr2) { // [O(n)]
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L30: Anagram Challenge Solution
+
+function validAnagram1(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup [letter] ? lookup [letter] += 1 : lookup [letter] = 1;
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second [i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup [letter]) {
+            return false;
+        } else {
+            lookup [letter] -= 1;
+        }
+    }
+
+    return true;
+}
+
+function validAnagram2(str1, str2){
+    if(str1.length !== str2.length) {
+        return false;
+    }
+    
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    
+    for(let char of str1) { // [O(n)]
+        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1; // === ++frequencyCounter1[char] || 1
+    }
+        
+    for(let char of str2) { // [O(n)]
+        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1; // === ++frequencyCounter2[char] || 1
+    }
+        
+    for(let key in frequencyCounter1) { // [O(n)]
+        if(!(key in frequencyCounter2)) {
+            return false
+        }
+
+        if(frequencyCounter2[key] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    
+    return true;
+    
+}
+
+/*---------------------------------------------------------------------------------------------------*/
