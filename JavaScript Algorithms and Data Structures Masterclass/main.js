4f540f838debbed3689f1f866b645d27c4614b3b
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-10-07 00:14:54 +0300

    JavaScript Algorithms and Data Structures Masterclass/S10:L66[Naive String Search Implementation]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1113,3 +1113,25 @@ function binarySearch(arr, elem) { // [O(log(n))]
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L66: Naive String Search Implementation
+
+function naiveSearch(long, short) {
+    let count = 0;
+
+    for (let i = 0; i < long.length; i++) {
+        for(let j = 0; j < short.length; j++) {
+            if(short[j] !== long[i + j]) {
+                break;
+            }
+
+            if(j === short.length - 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+/*---------------------------------------------------------------------------------------------------*/
