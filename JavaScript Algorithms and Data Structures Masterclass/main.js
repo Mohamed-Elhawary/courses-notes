053d65b8bfaf6b5ea0472c1239c164bf7162a37f
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-10-06 22:59:44 +0300

    JavaScript Algorithms and Data Structures Masterclass/S10:L63[Binary Search Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1044,3 +1044,27 @@ function linearSearch(arr, val) { // [O(n)]
         - Rather than eliminating one element at a time, you can eliminate half of the remaining elements at a time.
         - Binary search only works on sorted arrays!
 */
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L63: Binary Search Solution
+
+function binarySearch(arr, elem) { // [O(log(n))]
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+
+        middle = Math.floor((start + end) / 2);
+    }
+
+    return arr[middle] === elem ? middle : -1;
+}
+
+/*---------------------------------------------------------------------------------------------------*/
