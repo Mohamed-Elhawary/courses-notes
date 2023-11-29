beacd6b7bb1a9c739acf0297b18c2745af5258d3
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-11-29 18:21:58 +0200

    JavaScript Algorithms and Data Structures Masterclass/S16:L95[Quick Sort Implementation]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1760,3 +1760,28 @@ function pivot(arr, start = 0, end = arr.length - 1) {
 }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L95: Quick Sort Implementation
+
+/*
+    >> Quick Sort Implementation:
+        - Call the pivot helper on the array.
+        - When the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray to the left of that index, and the subarray to the right of that index.
+        - Your base case occurs when you consider a subarray with less than 2 elements.
+*/
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if(left < right) {
+        let pivotIndex = pivot(arr, left, right);
+
+        // Left
+        quickSort(arr, left, pivotIndex - 1);
+
+        // Right
+        quickSort(arr, pivotIndex + 1, right);
+    }
+
+    return arr;
+}
+
+/*---------------------------------------------------------------------------------------------------*/
