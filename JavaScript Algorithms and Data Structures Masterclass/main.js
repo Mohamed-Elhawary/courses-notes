1f2a603f69f737c6c5ba38d00a7c46dc40a0c8ed
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-11-30 19:33:36 +0200

    JavaScript Algorithms and Data Structures Masterclass/S16:L100[Radix Sort: Helper Methods]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1842,3 +1842,29 @@ quickSort([4, 6, 9, 1, 2, 5]);
         - It exploits the fact that information about the size of a number is encoded in the number of digits.
         - More digits means a bigger number!
 */
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L100: Radix Sort: Helper Methods
+
+function getDigit(num, place) { // returns the digit in num at the given place value
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) { // returns the number of digits in num
+    if(num === 0) return 1;
+
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) { // given an array of numbers, returns the number of digits in the largest numbers in the list
+    let maxDigits = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+
+    return maxDigits;
+}
+
+/*---------------------------------------------------------------------------------------------------*/
