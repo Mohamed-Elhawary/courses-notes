fd5bfbba6a3315c80fa681085a2ab0cef6a5f8ec
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-11-30 19:55:42 +0200

    JavaScript Algorithms and Data Structures Masterclass/S17:L102[Radix Sort: Implementation]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1884,3 +1884,27 @@ function mostDigits(nums) { // given an array of numbers, returns the number of 
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L102: Radix Sort: Implementation
+
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({length: 10}, () => []);
+
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k);
+
+            digitBuckets[digit].push(nums[i]);
+        }
+
+        nums = [].concat(...digitBuckets);
+    }
+
+    return nums;
+}
+
+radixSort([23, 345, 5467, 12, 2345, 9852]);
+
+/*---------------------------------------------------------------------------------------------------*/
