315d44c821f26f6d16718f8661302528bf6484d9
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-11-30 19:40:24 +0200

    JavaScript Algorithms and Data Structures Masterclass/S16:L101[Radix Sort: Pseudocode]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -1868,3 +1868,19 @@ function mostDigits(nums) { // given an array of numbers, returns the number of 
 }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L101: Radix Sort: Pseudocode
+
+/*
+    >> Radix Sort Pseudocode:
+        - Define a function that accepts list of numbers.
+        - Figure out how many digits the largest number has.
+        - Loop from k = 0 up to this largest number of digits.
+        - For each iteration of the loop:
+            • Create buckets for each digit (0 to 9).
+            • Place each number in the corresponding bucket based on its (kth) digit.
+        - Replace our existing array with values in our buckets, starting with 0 and going up to 9.
+        - Return list at the end!
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
