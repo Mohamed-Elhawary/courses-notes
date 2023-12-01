ab19e5758038fa173f197d97adf957380e9a3bcd
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-01 23:01:23 +0200

    JavaScript Algorithms and Data Structures Masterclass/S19:L110[Intro to Singly Linked Lists ]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -2107,3 +2107,38 @@ Point.distance(p1, p2);
         - Instance methods can be added to classes similar to methods in objects.
         - Class methods can be added using the (static) keyword.
 */
+
+/*---------------------------------------------------------------------------------------------------*/
+
+/* Section 19: Singly Linked Lists */
+
+// L110: Intro to Singly Linked Lists 
+
+/*
+    >> Objectives:
+        - Define what a singly linked list is.
+        - Compare and contrast linked lists with arrays.
+        - Implement insertion, removal and traversal methods on singly linked lists.
+
+    >> What is a singly linked list?
+        - A data structure that contains a head, tail and length property.
+        - Linked Lists consist of nodes, and each node has a value and a pointer to another node or null, each node is only connected one directionally to the next node.
+        - You can think of Arrays like skyscraper with elevators, you could say, take me to the fifth floor and it takes you right there.
+        - You can think of Linked Lists like skyscraper with no elevators, there is only stairs, and each set of each floor is connected to 
+          the next one by a set of stairs, so to get to the fifth floor, we have to start at the first one, take the stairs to the second,
+          then go up to the third, then to the fourth and then we get to the fifth.
+
+    >> Comparisons with Arrays:
+        - Linked Lists:
+            • Do not have indexes!
+            • Connected via nodes with a next pointer.
+            • Insertion and deletion can be very easy.
+            • Random access is not allowed.
+
+        - Arrays:
+            • Indexed in order!
+            • Insertion and deletion can be expensive.
+            • Can quickly be accessed at a specific index.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
