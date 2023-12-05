7965d9e2aaf3b9a9e77f0528232a354b21d1fe9e
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-05 17:03:28 +0200

    JavaScript Algorithms and Data Structures Masterclass/S18:L124[Singly Linked List: Insert Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -2885,3 +2885,176 @@ console.log(list6);
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L124: Singly Linked List: Insert Solution
+
+class SinglyLinkedList7 {
+    constructor(){
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this; // return "this" to be allowed to chain methods on the instance
+    }
+
+    pop() {
+        if(!this.head) return undefined;
+
+        let current = this.head;
+
+        let newTail = current;
+
+        while(current.next) {
+            newTail = current;
+
+            current = current.next;
+        }
+
+        this.tail = newTail;
+
+        this.tail.next = null;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.head = null;
+
+            this.tail = null;
+        }
+
+        return current; // This is the Popped value that we removed from the list
+    }
+
+    shift() {
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+
+        this.head = currentHead.next;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.tail = null;
+        }
+
+        return currentHead; // This is the Shifted value that we removed from the list
+    }
+
+    unshift() {
+        const newNode = new Node(val);
+
+        if(!this.head) {
+            this.head = newNode;
+
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+
+            this.head = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+
+        let counter = 0;
+
+        let current = this.head;
+
+        while(counter !== index) {
+            current = current.next;
+
+            counter++;
+        }
+
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+
+        if(foundNode) {
+            foundNode.val = val;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+
+        if(index === this.length) return !!this.push(val);
+
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);
+
+        let prev = this.get(index - 1);
+
+        let temp = prev.next;
+
+        prev.next = newNode;
+
+        newNode.next = temp;
+
+        this.length++;
+
+        return true;
+    }
+}
+
+
+let list7 = new SinglyLinkedList7();
+
+list7.push("Hello");
+
+list7.push("Goodbye");
+
+list7.push("!");
+
+list7.insert(1, "Colt"); // true
+
+console.log(list7);
+
+/*
+    { 
+        val: "Hello", 
+        next: { 
+            val: "Colt", 
+            next: { 
+                val: "Goodbye", 
+                next: { 
+                    val: "!", 
+                    next: null 
+                } 
+            } 
+        } 
+    }
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
