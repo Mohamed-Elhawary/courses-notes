0161a45af086adc6a75778a3d15faf3244edb252
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-05 13:59:17 +0200

    JavaScript Algorithms and Data Structures Masterclass/S18:L116[Singly Linked List: Shift Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -2380,3 +2380,81 @@ list3.pop(); // { val: "!", next: null }
         - Decrement the length by 1.
         - Return the value of the node removed.
 */
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L115: Singly Linked List: Shift Solution
+
+class SinglyLinkedList4 {
+    constructor(){
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this; // return "this" to be allowed to chain methods on the instance
+    }
+
+    pop() {
+        if(!this.head) return undefined;
+
+        let current = this.head;
+
+        let newTail = current;
+
+        while(current.next) {
+            newTail = current;
+
+            current = current.next;
+        }
+
+        this.tail = newTail;
+
+        this.tail.next = null;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.head = null;
+
+            this.tail = null;
+        }
+
+        return current; // This is the Popped value that we removed from the list
+    }
+
+    shift() {
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+
+        this.head = currentHead.next;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.tail = null;
+        }
+
+        return currentHead;
+    }
+}
+
+/*---------------------------------------------------------------------------------------------------*/
