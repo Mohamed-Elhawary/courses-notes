c11764faae24613b2611e958a7b58a56e68e2898
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-05 15:58:50 +0200

    JavaScript Algorithms and Data Structures Masterclass/S18:L121[Singly Linked List: Set Intro]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -2701,3 +2701,17 @@ list5.push("Goodbye");
 list5.push("!");
 
 list5.get(1); // { val: "Goodbye", next: { val: "!", next: null } }
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L121: Singly Linked List: Set Intro
+
+/*
+    >> Set Pseudocode:
+        - This function should accept a value and an index.
+        - Use your get function to find the specific node.
+        - If the node is not found, return false.
+        - If the node is found, set the value of that node to be the value passed to the function and return true.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
