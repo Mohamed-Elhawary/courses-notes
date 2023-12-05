88c1a9e939667a1d7f195221a6e620e76554caea
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-05 15:16:32 +0200

    JavaScript Algorithms and Data Structures Masterclass/S18:L119[Singly Linked List: Get Intro]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -2568,3 +2568,14 @@ class SinglyLinkedList5 {
 }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L119: Singly Linked List: Get Intro
+
+/*
+    >> Get Pseudocode:
+        - This function should accept an index.
+        - If the index is less than zero or greater than or equal to the length of the list, return null.
+        - Loop through the list until you reach the index and return the node at that specific index.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
