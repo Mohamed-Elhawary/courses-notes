699f81b4d7fdadef10fc58963f46e5f36df239bf
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-05 17:20:41 +0200

    JavaScript Algorithms and Data Structures Masterclass/S18:L126[Singly Linked List: Remove Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3059,7 +3059,7 @@ console.log(list7);
 
 /*---------------------------------------------------------------------------------------------------*/
 
-// L125: Singly Linked List: Remove Solution
+// L125: Singly Linked List: Remove Intro
 
 /*
     >> Remove Pseudocode:
@@ -3070,6 +3070,190 @@ console.log(list7);
         - Set the next property on that node to be the next of the next node.
         - Decrement the length.
         - Return the value of the node removed.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L126: Singly Linked List: Remove Solution
+
+class SinglyLinkedList8 {
+    constructor(){
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this; // return "this" to be allowed to chain methods on the instance
+    }
+
+    pop() {
+        if(!this.head) return undefined;
+
+        let current = this.head;
+
+        let newTail = current;
+
+        while(current.next) {
+            newTail = current;
+
+            current = current.next;
+        }
+
+        this.tail = newTail;
+
+        this.tail.next = null;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.head = null;
+
+            this.tail = null;
+        }
+
+        return current; // This is the Popped value that we removed from the list
+    }
+
+    shift() {
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+
+        this.head = currentHead.next;
+
+        this.length--;
+
+        if(this.length === 0) {
+            this.tail = null;
+        }
+
+        return currentHead; // This is the Shifted value that we removed from the list
+    }
+
+    unshift() {
+        const newNode = new Node(val);
+
+        if(!this.head) {
+            this.head = newNode;
+
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+
+            this.head = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+
+        let counter = 0;
+
+        let current = this.head;
+
+        while(counter !== index) {
+            current = current.next;
+
+            counter++;
+        }
+
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+
+        if(foundNode) {
+            foundNode.val = val;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+
+        if(index === this.length) return !!this.push(val);
+
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);
+
+        let prev = this.get(index - 1);
+
+        let temp = prev.next;
+
+        prev.next = newNode;
+
+        newNode.next = temp;
+
+        this.length++;
+
+        return true;
+    }
+    
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+
+        if(index === 0) return this.shift();
+
+        if(index === this.length - 1) return this.pop();
+
+        let previousNode = this.get(index - 1);
+
+        let removed = previousNode.next;
+
+        previousNode.next = removed.next;
+
+        this.length--;
+
+        return removed;
+    }
+}
+
+let list8 = new SinglyLinkedList8();
+
+list8.push("Hello");
+
+list8.push("Goodbye");
+
+list8.push("!");
+
+list8.remove(1); // { val: "Goodbye", next: { val: "!", next: null } }
+
+console.log(list8);
+
+/*
+    { 
+        val: "Hello", 
+        next: { 
+            val: "!", 
+            next: null 
+        } 
+    }
 */
 
 /*---------------------------------------------------------------------------------------------------*/
