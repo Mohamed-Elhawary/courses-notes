0abe556a3e5122b0f27355bffbe27b866c5a9f51
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-23 18:15:58 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L137[Shift]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3286,3 +3286,21 @@ doublyList2.push("!");
 singlyList2.pop(); // { val: "!", next: null, prev: null }
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L137: Shift
+
+/*
+    >> Shifting Pseudocode:
+        - If length is 0, return undefined.
+        - Store the current head property in a variable (we'll call it old head).
+        - If the length is one:
+            • Set the head to be null.
+            • Set the tail to be null.
+        - Update the head to be the next of the old head.
+        - Set the head's prev property to null.
+        - Set the old head's next to null.
+        - Decrement the length.
+        - Return old head.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
