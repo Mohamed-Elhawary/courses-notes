b24c330d44fcfd63a7b45be39a79e68d6c14fbe6
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-23 17:37:12 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L135[Pop]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3205,3 +3205,18 @@ console.log(doublyList1);
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L135: Pop
+
+/*
+    >> Popping Pseudocode:
+        - If there is no head, return undefined.
+        - Store the current tail in a variable to return later.
+        - If the length is 1, set the head and tail to be null.
+        - Update the tail to be the previous Node.
+        - Set the newTail's next to null.
+        - Decrement the length.
+        - Return the value removed.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
