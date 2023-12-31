2500805a03667cff400bdcf596e54be109717cfb
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-31 15:30:46 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L145[Insert]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3708,3 +3708,17 @@ doublyList6.set(7, "???"); // false
 
 /*---------------------------------------------------------------------------------------------------*/
 
+// L145: Insert
+
+/*
+    >> Insert Pseudocode:
+        - If the index is less than zero or greater than or equal to the length return false.
+        - If the index is 0, unshift.
+        - If the index is the same as the length, push.
+        - Use the get method to access the index -1.
+        - Set the next and prev properties on the correct nodes to link everything together.
+        - Increment the length.
+        - Return true.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
