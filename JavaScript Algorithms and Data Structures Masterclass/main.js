9f87f43a9acda574bae2ada6a1701402f16211e3
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-31 15:15:20 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L143[Set]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3605,3 +3605,15 @@ doublyList5.get(3);
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L143: Set
+
+/*
+    >> Set Pseudocode:
+        - Create a variable which is the result of the get method at the index passed to the function.
+            • If the get method returns a valid node, set the value of that node to be the value passed to the function.
+            • Return true.
+        - Otherwise, return false.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
