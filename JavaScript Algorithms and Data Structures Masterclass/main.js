8e8932e094f333fcd0be2c7edc59d8fa97e7a7f5
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-31 15:24:33 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L144[Set Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3617,3 +3617,94 @@ doublyList5.get(3);
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L144: Set Solution
+
+class DoublyLinkedList6 {
+    constructor() {
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new DoublyNode(val);
+
+        if (this.length === 0) {
+            this.head = newNode;
+
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+
+            newNode.prev = this.tail;
+
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+
+        let counter, current;
+
+        if (index <= this.length / 2) {
+            counter = 0;
+
+            current = this.head;
+
+            while (counter !== index) {
+                current = current.next;
+
+                counter++;
+            }
+        } else {
+            counter = this.length - 1;
+
+            current = this.tail;
+
+            while (counter !== index) {
+                current = current.prev;
+
+                counter--;
+            }
+        }
+
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+
+        if (foundNode) {
+            foundNode.val = val;
+
+            return true;
+        }
+
+        return false;
+    }
+}
+
+const doublyList6 = new DoublyLinkedList6();
+
+doublyList6.push("Hello");
+
+doublyList6.push("Goodbye");
+
+doublyList6.push("!");
+
+doublyList6.push("Colt");
+
+doublyList6.set(2, "!!!"); // true
+
+doublyList6.set(7, "???"); // false
+
+/*---------------------------------------------------------------------------------------------------*/
+
