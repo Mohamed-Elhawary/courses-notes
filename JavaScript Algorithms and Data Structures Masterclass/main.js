fa17fcb16660e281e2198e41c1c05d58a8839bc0
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2023-12-31 16:57:42 +0200

    JavaScript Algorithms and Data Structures Masterclass/S20:L148[Remove Solution]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -3951,3 +3951,133 @@ console.log(doublyList7);
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L148: Remove Solution
+
+class DoublyLinkedList8 {
+    constructor() {
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new DoublyNode(val);
+
+        if (this.length === 0) {
+            this.head = newNode;
+
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+
+            newNode.prev = this.tail;
+
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        const poppedNode = this.tail;
+
+        if (this.length === 1) {
+            this.head = null;
+
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+
+            this.tail.next = null;
+        }
+
+        poppedNode.prev = null;
+
+        this.length--;
+
+        return poppedNode;
+    }
+
+    shift() {
+        if (this.length === 0) return undefined;
+
+        const oldHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+
+            this.head.prev = null;
+        }
+
+        oldHead.next = null;
+
+        this.length--;
+
+        return oldHead;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+
+        let counter, current;
+
+        if (index <= this.length / 2) {
+            counter = 0;
+
+            current = this.head;
+
+            while (counter !== index) {
+                current = current.next;
+
+                counter++;
+            }
+        } else {
+            counter = this.length - 1;
+
+            current = this.tail;
+
+            while (counter !== index) {
+                current = current.prev;
+
+                counter--;
+            }
+        }
+
+        return current;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+
+        if (index === 0) return this.shift();
+
+        if (index === this.length - 1) return this.pop();
+
+        let removedNode = this.get(index);
+
+        removedNode.prev.next = removedNode.next;
+
+        removedNode.next.prev = removedNode.prev;
+
+        removedNode.next = null;
+
+        removedNode.prev = null;
+
+        this.length--;
+
+        return removedNode;
+    }
+}
+
+/*---------------------------------------------------------------------------------------------------*/
