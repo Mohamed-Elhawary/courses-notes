e3174fa5c7811d23fa6673b07be2a6ea00de0256
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-10 22:10:20 +0200

    JavaScript Algorithms and Data Structures Masterclass/S21:L157[Writing Our Own Queue From Scratch]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -4370,3 +4370,82 @@ queue2.pop(); // THIRD
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L157: Writing Our Own Queue From Scratch
+
+/*
+    >> Enqueue Pseudocode:
+        - This function accepts some value.
+        - Create a new node using that value passed to the function.
+        - If there are no nodes in the queue, set this node to be the first and last property of the queue.
+        - Otherwise, set the next property on the current last to be that node, and then set the last property of the queue to be that node.
+        - Increment the size of the queue by 1.
+
+    >> Dequeue Pseudocode:
+        - If there is no first property, just return null.
+        - Store the first property in a variable.
+        - See if the first is the same as the last (check if there is only 1 node). If so, set the first and last to be null.
+        - If there is more than 1 node, set the first property to be the next property of first.
+        - Decrement the size by 1.
+        - Return the value of the node dequeued.
+*/
+
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+
+        this.last = null;
+
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new QueueNode(val);
+
+        if (!this.first) {
+            this.first = newNode;
+
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+
+            this.last = newNode;
+        }
+
+        return ++this.size;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+
+        let temp = this.first;
+
+        if (this.first === this.last) {
+            this.last = null;
+        }
+
+        this.first = this.first.next;
+
+        this.size--;
+
+        return temp.val;
+    }
+}
+
+/*
+    >> Why we do "enqueue" & "dequeue" at this way like we do "push" & "shift" in Singly Linked List?
+        - Because we want to have a constant time complexity, but removing from the end is not constant time because we have to 
+          iterate through the entire list to get to the item before the last item and set it to be the new last, and Queue is supposed 
+          to be constant time at removing and adding, so we are adding from the end and removing from beginning of our list, but we are calling it 
+          "enqueue" and "dequeue" because it is a Queue.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
