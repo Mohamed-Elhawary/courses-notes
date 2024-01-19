806b4670756554fac52e5a0885e259f61fe45ecc
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-19 23:46:19 +0200

    JavaScript Algorithms and Data Structures Masterclass/S23:L175[Depth First PreOrder Intro]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -4872,7 +4872,6 @@ tree3.contains(99); // false
 
 // L174: Breadth First Search Solution
 
-
 class BinarySearchTree4 {
     constructor() {
         this.root = null;
@@ -4952,5 +4951,21 @@ tree4.insert(8);
 tree4.insert(20);
 
 tree4.BFS(); // [10, 6, 15, 3, 8, 20]
+
+/*---------------------------------------------------------------------------------------------------*/
+
+// L175: Depth First PreOrder Intro
+
+/*
+    >> DFS - PreOrder Pseudocode: 
+        - Create a variable to store the values of nodes visited.
+        - Store the root of the BST in a variable called current.
+        - Write a helper function which accepts a node.
+            • Push the value of the node to the variable that stores the values.
+            • If the node has a left property, call the helper function with the left property on the node.
+            • If the node has a right property, call the helper function with the right property on the node.
+        - Invoke the helper function with the current variable.
+        - Return the array of values.
+*/
 
 /*---------------------------------------------------------------------------------------------------*/
