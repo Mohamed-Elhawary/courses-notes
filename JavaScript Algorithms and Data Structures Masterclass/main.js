fc85af8ada8fd7d850f7b673c1abfcd3c9633ecc
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-23 19:03:51 +0200

    JavaScript Algorithms and Data Structures Masterclass/S23:L177[Depth First PostOrder]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -5047,3 +5047,19 @@ tree5.insert(20);
 tree5.DFSPreOrder(); // [10, 6, 3, 8, 15, 20]
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L177: Depth First PostOrder Intro
+
+/*
+    >> DFS - PostOrder Pseudocode: 
+        - Create a variable to store the values of nodes visited.
+        - Store the root of the BST in a variable called current.
+        - Write a helper function which accepts a node.
+            • If the node has a left property, call the helper function with the left property on the node.
+            • If the node has a right property, call the helper function with the right property on the node.
+            • Push the value of the node to the variable that stores the values.
+        - Invoke the helper function with the current variable.
+        - Return the array of values.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
