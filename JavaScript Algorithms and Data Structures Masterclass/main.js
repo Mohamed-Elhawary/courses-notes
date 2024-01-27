84ebcda87399782bc45361836abf84cd6c6d27da
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-28 01:34:09 +0200

    JavaScript Algorithms and Data Structures Masterclass/S24:L183[Intro to Heaps]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -5257,3 +5257,36 @@ tree7.DFSInOrder(); // [3, 6, 8, 10, 15, 20]
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+/* Section 24: Binary Heaps */
+
+// L183: Intro to Heaps
+
+/*
+    >> Objectives:
+        - Define what a binary heap is.
+        - Compare and contrast min and max heaps.
+        - Implement basic methods on heaps.
+        - Understand where heaps are used in the real world and what other data structures can be constructed from heaps.
+
+    >> What is Binary Heap ?
+        - Very similar to a binary search tree, but with some different rules!
+        - In a MaxBinaryHeap, parent nodes are always larger than child nodes.
+        - In a MinBinaryHeap, parent nodes are always smaller than child nodes.
+        - Binary Heaps are as compact as possible. All the children of each node are as full as they can be and left children are filled out first.
+        - No Implied ordering between Siblings.
+        - Used to implement Priority Queues, which are very commonly used data structure.
+        - They are also used quite a bit, with graph traversal algorithms.
+
+    >> Max Binary Heap:
+        - Each parent has at most two child nodes.
+        - The value of each parent node is always greater than its child nodes.
+        - In a max Binary Heap the parent is greater than the children, but there are no guarantees between sibling nodes.
+
+    >> Min Binary Heap:
+        - Each parent has at most two child nodes.
+        - The value of each parent node is always smaller than its child nodes.
+        - In a max Binary Heap the parent is smaller than the children, but there are no guarantees between sibling nodes.
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
