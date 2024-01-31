d108fb142e959d63b40518cf872ee36e9036939d
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-31 22:19:39 +0200

    JavaScript Algorithms and Data Structures Masterclass/S24:L187[Heap: ExtractMax Intro]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -5382,3 +5382,30 @@ heap.insert(55);
 console.log(heap.values); // [55, 39, 41, 18, 27, 12, 33]
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L187: Heap: ExtractMax Intro
+
+/*
+    >> Removing from a Heap:
+        - Remove the Root
+        - Replace with the most recently added.
+        - Adjust (Sink Down).
+
+    >> What is Sink Down?
+        - The procedure for deleting the root from the heap (effectively extracting the maximum element in a max-heap or the minimum element in a min-heap) 
+          and restoring the properties is called down-heap (also known as bubble-down, percolate-down, sift-down, trickle down, heapify-down, cascade-down, and extract-min/max).
+
+    >> Removing (Extract Max) Pseudocode:
+        - Swap the first value in the values property with the last one.
+        - Pop from the values property, so you can return the value at the end.
+        - Have the new root "sink down" to the correct spot...
+            • Your parent index starts at 0 (the root).
+            • Find the index of the left child: 2 * index + 1 (make sure its not out of bounds).
+            • Find the index of the right child: 2 * index + 2 (make sure its not out of bounds).
+            • If the left or right child is greater than the element...swap. If both left and right children are larger, swap with the largest child.
+            • The child index you swapped to now becomes the new parent index.
+            • Keep looping and swapping until neither child is larger than the element.
+            • Return the old root!
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
