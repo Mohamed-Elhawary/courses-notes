2c5b6ce417ad42e6cff52904242428ce63e2c247
Mohamed Elhawary <mohamed.k.elhawary@gmail.com>
2024-01-31 21:30:11 +0200

    JavaScript Algorithms and Data Structures Masterclass/S24:L185[Heap: Insert Intro]

--- JavaScript Algorithms and Data Structures Masterclass/main.js
+++ JavaScript Algorithms and Data Structures Masterclass/main.js
@@ -5302,3 +5302,18 @@ tree7.DFSInOrder(); // [3, 6, 8, 10, 15, 20]
 */
 
 /*---------------------------------------------------------------------------------------------------*/
+
+// L185: Heap: Insert Intro
+
+/*
+    >> Insert Pseudocode:
+        - Push the value into the values property on the heap.
+        - Bubble Up:
+            • Create a variable called index which is the length of the values property - 1.
+            • Create a variable called parentIndex which is the floor of (index - 1)/2.
+            • Keep looping as long as the values element at the parentIndex is less than the values element at the child index.
+                • Swap the value of the values element at the parentIndex with the value of the element property at the child index.
+                • Set the index to be the parentIndex, and start over!
+*/
+
+/*---------------------------------------------------------------------------------------------------*/
